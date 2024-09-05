5b500fcc3b333f613820822ccff0f3add335a50f
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-09-05 09:50:07 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,29 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
-        System.out.println("Hello world!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        // Mes gribam uzzinat no lietitaja: Vardu, Uzvardu, Gruppu?\
+
+        // Un beigas printet visu kopa viena rinda 
+
+        System.out.println("Ievadi savu vardu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvardu: ");
+        String uzname = scanner.nextLine();
+
+        System.out.println("Ievadi savu gruppu: ");
+        String grup = scanner.nextLine();
+
+
+        System.out.println(name + " " + uzname + " " + grup);
     }
 
 }
