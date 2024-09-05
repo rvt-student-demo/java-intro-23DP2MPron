9405d1fc171d8ab2dc62f322159308163c54adc9
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-09-05 10:28:10 +0000

    Various Variables

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,21 +9,23 @@ public class App
         
         Scanner scanner = new Scanner(System.in);
 
-        // Mes gribam uzzinat no lietitaja: Vardu, Uzvardu, Gruppu?\
+        System.out.println("Chiken:");
+        String chiken = scanner.nextLine();
+        System.out.println("Bacon:");
+        String bacon = scanner.nextLine();
+        System.out.println("Tractor:");
+        String tractor = scanner.nextLine();
 
-        // Un beigas printet visu kopa viena rinda 
-
-        System.out.println("Ievadi savu vardu: ");
-        String name = scanner.nextLine();
-
-        System.out.println("Ievadi savu uzvardu: ");
-        String uzname = scanner.nextLine();
-
-        System.out.println("Ievadi savu gruppu: ");
-        String grup = scanner.nextLine();
-
-
-        System.out.println(name + " " + uzname + " " + grup);
+        System.out.println("Chicken:");
+        System.out.println( chiken);
+        System.out.println("Bacon:");
+        System.out.println(bacon);
+        System.out.println("Tractor:");
+        System.out.println(tractor);
+        System.out.println("And finally, a summary:" );
+        System.out.println( chiken);
+        System.out.println(bacon);
+        System.out.println(tractor);
     }
 
 }
