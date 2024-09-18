e57c58a6708a88acf4fcfd784e72567a722ddd1a
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-09-18 10:31:41 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,19 +8,42 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ievadi savu vardu:");
+        System.out.println("Give points:");
+        int grade = Integer.valueOf(scanner.nextLine());
+        if (grade < 0) {
+            System.out.println("impossible!");
+        }
 
-        String name = scanner.nextLine();
+        else if (grade < 50) {
+            System.out.println("failed");
+        }
 
-        System.out.println("Ievadi savu uzvardu:");
-        String lastname = scanner.nextLine();
+        else if (grade < 60) {
+            System.out.println("1");
+        }
 
-        System.out.println("Ievadi savu grupu:");
-        String group = scanner.nextLine();
+        else if (grade < 70) {
+            System.out.println("2");
+        }
 
-        System.out.println("Students: " + name + " " + lastname + ", group: " + group );
+        else if (grade < 80) {
+            System.out.println("3");
+        }
 
+        else if (grade < 90) {
+            System.out.println("4");
+        }
 
+        else if (grade <= 100) {
+            System.out.println("5");
+        }
+
+        else if (grade > 100) {
+            System.out.println("incredible!");
+        }
+
+        
     }
+
 
 }
