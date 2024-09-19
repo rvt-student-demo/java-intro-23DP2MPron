15ab7f36ee95b98bcee116fbba2576a891880c58
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-09-19 10:27:43 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,36 +6,29 @@ public class App
 {
     public static void main( String[] args )
     {
+    
+        Scanner scanner = new Scanner(System.in);
         
-        Scanner reader = new Scanner(System.in);
 
-        System.out.println("Value of the gift?");
-        int gift = Integer.valueOf(reader.nextLine());
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
 
-        if (gift < 5000) {
-            System.out.println("No tax!");
-        }
+            if (number >= 0) {
+                System.out.print(number * number);
+                break;
+            }
+
+            if (number < 0) {
+                System.out.println("Unsuitable number"); 
+                continue;
+               
+            }
+
         
-        else if (gift < 25000) {
-            System.out.println(100 + (gift - 5000) * 0.08);
-        }
-
-        else if (gift < 55000) {
-            System.out.println(1700 + (gift - 25000) * 0.1);
-        }
-
-        else if (gift < 200000) {
-            System.out.println(4700 + (gift - 55000) * 0.12);
-        }
-
-        else if (gift < 1000000) {
-            System.out.println(22100 + (gift - 200000) * 0.15);
-        }
-
-        else if (gift >= 1000000) {
-            System.out.println(142100 + (gift - 1000000) * 0.17);
-        }
     }
 
+    
+}
 
 }
