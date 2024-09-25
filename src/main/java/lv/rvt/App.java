22af42838d6dc1221935bba5b8fe6aa0bb9f5893
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-09-25 10:18:46 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,18 +9,21 @@ public class App
     
         Scanner scanner = new Scanner(System.in);
         
-
+        int chet = 0;
+        int sum = 0;            
         while (true) {
             System.out.println("Give a number:");
             int number = Integer.valueOf(scanner.nextLine());
-
-            if (number >= 0) {
-                System.out.print(number * number);
+            
+            if (number == 0) {
+                System.out.print(sum + " ");
+                System.out.print(chet);
                 break;
             }
 
-            if (number < 0) {
-                System.out.println("Unsuitable number"); 
+            else {
+                chet = chet + 1;
+                sum = sum + number; 
                 continue;
                
             }
