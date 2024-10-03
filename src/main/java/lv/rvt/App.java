d89ba70cf43b14cc0298f208fc253f3c426ce356
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-10-03 08:26:37 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,33 +2,24 @@ package lv.rvt;
 
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    
+public class App {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Give a number:");
         int number = Integer.valueOf(scanner.nextLine());
-        printText(number);
-        
-
-    
+        System.out.println("Give a 2 number:");
+        int number2 = Integer.valueOf(scanner.nextLine());
+        printText(number, number2);
     }
 
-    public static void printText(int number){
-        int i = 1;
-        while (i <= number) {
-            System.out.println(i);
-            i++;
-           
+    public static void printText(int number, int number2) {
+        for (int i = number; i <= number2; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
         }
-            
-            
-        
     }
-
 }
 
 
