304a14266a69f6f6075ad8f36c84fab03c740d30
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-10-16 10:20:32 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,33 @@
 package lv.rvt;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Stars.printTriangle(5);
-        Stars.printRectangle(3,4);
-        Stars.printSquare(4);
-        Stars.printStars(5);
-    }
+        List<Integer> numbers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-    
+        while (true) {
+            System.out.print("Enter an integer (0 to stop): ");
+            int num = scanner.nextInt();
+            if (num == 0) {
+                break;
+            }
+            numbers.add(num);
+        }
+
+        if (numbers.size() >= 3) {
+            int sum = numbers.get(1) + numbers.get(2);
+            System.out.println("The sum of the second and third numbers is: " + sum);
+        }
+        int summa = 0;
+        for (int num : numbers) {
+            summa += num;
+        }
+        System.out.println("Sum: " + summa);
+    }
 }
 
 
