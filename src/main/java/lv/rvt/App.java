6e25db548d1c2103688f9b0821d3fc68bf95be4c
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-11-06 12:17:51 +0000

    Statistics

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,12 +1,28 @@
 package lv.rvt;
+import java.rmi.server.SocketSecurityException;
 import java.util.*;
 public class App {
     public static void main(String[] args) {
-        Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
+        Scanner scanner = new Scanner(System.in);
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+            Statistics statistics = new Statistics();{
+            System.out.println("Enater numbers:");
+            while (true) {
+                int input = Integer.valueOf(scanner.nextLine());
+                if (input == -1) {
+                    break;
+                }
+                if (input %2 == 0){
+                    even.addNumber(input);
+                }
+                else{
+                    odd.addNumber(input);
+                }
+                statistics.addNumber(input);
+            }
+        }   System.out.println("Sum " + statistics.sum());
+            System.out.println("Sum of even numbers: " + even.sum());
+            System.out.println("Sum of odd numbers: " + odd.sum());
     }
 }
