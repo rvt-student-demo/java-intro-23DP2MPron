00686c12e6a076ca24d9b702fbd6822706d0db99
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-11-06 12:05:35 +0000

    Statistics

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,21 +2,11 @@ package lv.rvt;
 import java.util.*;
 public class App {
     public static void main(String[] args) {
-        Person matti = new Person("Matti");
-        Person juhana = new Person("Juhana");
-        Person es = new Person("Maksym");
-
-        es.setHeight(182);
-        es.setWeight(87);
-    
-        matti.setHeight(180);
-        matti.setWeight(86);
-    
-        juhana.setHeight(175);
-        juhana.setWeight(64);
-    
-        System.out.println(matti.getName() + ", body mass index is " + matti.bodyMassIndex());
-        System.out.println(juhana.getName() + ", body mass index is " + juhana.bodyMassIndex());
-        System.out.println(es.getName() + ", body mass index is " + es.bodyMassIndex());
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
     }
 }
