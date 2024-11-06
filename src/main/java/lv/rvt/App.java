0f01096c5a533e9f86ae68a1630527cc6ddca6dd
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-11-06 11:50:22 +0000

    Person

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,15 +1,22 @@
 package lv.rvt;
 import java.util.*;
 public class App {
-    public class  MainProgram  {
-        public static void main(String[] args){
-    Agent bond = new Agent("James", "Bond");
+    public static void main(String[] args) {
+        Person matti = new Person("Matti");
+        Person juhana = new Person("Juhana");
+        Person es = new Person("Maksym");
 
-bond.toString(); // prints nothing
-System.out.println(bond);
-
-Agent ionic = new Agent("Ionic", "Bond");
-System.out.println(ionic);
-        }
+        es.setHeight(182);
+        es.setWeight(87);
+    
+        matti.setHeight(180);
+        matti.setWeight(86);
+    
+        juhana.setHeight(175);
+        juhana.setWeight(64);
+    
+        System.out.println(matti.getName() + ", body mass index is " + matti.bodyMassIndex());
+        System.out.println(juhana.getName() + ", body mass index is " + juhana.bodyMassIndex());
+        System.out.println(es.getName() + ", body mass index is " + es.bodyMassIndex());
     }
 }
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -4,18 +4,31 @@ import java.util.*;
 public class Person {
     private String name;
     private int age;
-        public Person(String initialName) {
-            this.age = 0;
-            this.name = initialName;
-        }
-    
-        public void printPerson() {
-            System.out.println(this.name + ", age " + this.age + " years");
-        }
-    
-        // growOlder() method has been added
-        public void growOlder() {
-            this.age = this.age + 1;
+    private int weight;
+    private int height;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.weight = 0;
+        this.height = 0;
+        this.name = initialName;
+    }
+    public String getName() {
+        return this.name;
         
     }
+    
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heigthPerHundred = this.height / 100.0;
+        return this.weight / (heigthPerHundred * heigthPerHundred);
+    }
+
 }
