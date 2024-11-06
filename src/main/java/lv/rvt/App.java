0af95ec2969a2b904d21c169b27f2274657a7fb6
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-11-06 11:05:04 +0000

    DecreasingCounter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,34 +1,17 @@
 package lv.rvt;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
 public class App {
-    public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+    public class MainProgram {
+        public static void main(String[] args) {
+            DecreasingCounter counter = new DecreasingCounter(100);
 
-        while (true) {
-            System.out.print("Enter an integer (0 to stop): ");
-            int num = scanner.nextInt();
-            if (num == 0) {
-                break;
-            }
-            numbers.add(num);
-        }
+        counter.printValue();
 
-        if (numbers.size() >= 3) {
-            int sum = numbers.get(1) + numbers.get(2);
-            System.out.println("The sum of the second and third numbers is: " + sum);
+        counter.reset();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
         }
-        int summa = 0;
-        for (int num : numbers) {
-            summa += num;
-        }
-        System.out.println("Sum: " + summa);
     }
 }
-
-
-
