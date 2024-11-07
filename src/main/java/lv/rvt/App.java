3e363dd710be3e542a72c98489eeac6920c5d73b
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-11-07 08:51:15 +0000

    payment card part 4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,13 +4,16 @@ import java.util.*;
 public class App {
     public class MainProgram {
         public static void main(String[] args) {
-            PaymentCard card = new PaymentCard(5);
+            PaymentCard card = new PaymentCard(10);
             System.out.println(card);
     
-            card.eatHeartily();
+            card.addMoney(15);
             System.out.println(card);
     
-            card.eatHeartily();
+            card.addMoney(10);
+            System.out.println(card);
+    
+            card.addMoney(200);
             System.out.println(card);
         }
     }
--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -9,10 +9,11 @@ public class PaymentCard {
 
 
     public void eatAffordably() {
+        this.balance -= 2.60;
         if (balance < 2.60){
             this.balance += 0;
         }else{
-        this.balance -= 2.60;
+            this.balance -= 2.60;
         }
     }
 
@@ -23,12 +24,23 @@ public class PaymentCard {
         }
         else{
         this.balance -= 4.60;
+    }}
+
+    public void addMoney(double amount) {
+        if (balance <= 150){
+            this.balance += amount;
+            if(amount >=150){
+                this.balance = 150;
+            }
+        }
+        else{
+        this.balance += 0;
         }
     }
-
 
     
     public String toString() {
         return "The card has a balance of " + balance + " euros";
     }
 }
+
