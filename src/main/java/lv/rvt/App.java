9461a63165f4becc154750f06900931a62d8db8c
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-11-26 19:43:56 +0000

    Counter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,19 +5,19 @@ import java.nio.file.StandardOpenOption;
 
 public class App {
     public static void main(String[] args) {
-        Timer timer = new Timer(0, 0); 
-
-        while (true) {
-            System.out.println(timer);
-            timer.advance();
-
-            try {
-                Thread.sleep(10); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        Counter count = new Counter(15);
+        count.increase();
+        System.out.println(count);
+        count.increase(10);
+        System.out.println(count);
+        count.increase(-10);
+        System.out.println(count);
+        count.decrease();
+        System.out.println(count);
+        count.decrease(11);
+        System.out.println(count);
+        count.decrease(-11);
+        System.out.println(count);
     }
-}
-    
 
+}
