b7e9bf62c51f8571b391feb06e4c18dbf7a9fd51
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-11-28 11:24:22 +0000

    Health Station part 3

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -1,13 +1,19 @@
 package lv.rvt;
 
 public class HealthStation {
+    private int weighingCount;
     public int weigh(Person person) {
+        weighingCount ++;
         int weight = person.getWeight();
         return weight;
     }
+
     public void feed(Person person){
-        person.increaseWeight(1);
-        
+        person.increaseWeight(1); 
+    }
+
+    public int weighings() {
+        return weighingCount;
     }
 
 }
