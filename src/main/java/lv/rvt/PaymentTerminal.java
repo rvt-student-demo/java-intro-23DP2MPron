e6579d57891f5a53a922321fdec26cb9ab26cbe9
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-12-03 19:30:44 +0000

    Card payment part 3 card payments

--- src/main/java/lv/rvt/PaymentTerminal.java
+++ src/main/java/lv/rvt/PaymentTerminal.java
@@ -6,7 +6,6 @@ public class PaymentTerminal {
     private int heartyMeals;  
 
     public PaymentTerminal() {
-       
         this.money = 1000.0;
         this.affordableMeals = 0;
         this.heartyMeals = 0;
@@ -30,6 +29,24 @@ public class PaymentTerminal {
             return payment - mealCost; 
         }
         return payment; 
+    }
+
+    public boolean eatAffordably(PaymentCard card) {
+        double mealCost = 2.50; 
+        if (card.takeMoney(mealCost)) { 
+            this.affordableMeals++; 
+            return true; 
+        }
+        return false;
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        double mealCost = 4.30; 
+        if (card.takeMoney(mealCost)) { 
+            this.heartyMeals++; 
+            return true; 
+        }
+        return false; 
     }
 
     public String toString() {
