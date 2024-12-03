7f963ffa74efd16af1766d1116262c7e22175154
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-12-03 19:12:49 +0000

    Card payments part 1 "Dumb" payment card

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -3,27 +3,24 @@ package lv.rvt;
 public class PaymentCard {
     private double balance;
 
-   
-    public PaymentCard(double openingBalance) {
-        this.balance = openingBalance;
+    public PaymentCard(double balance) {
+        this.balance = balance;
     }
 
-    
-    public void eatAffordably() {
-        this.balance -= 2.60;
+    public double balance() {
+        return this.balance;
     }
 
-   
-    public void eatHeartily() {
-        this.balance -= 4.60;
+    public void addMoney(double increase) {
+        this.balance = this.balance + increase;
     }
 
-    
-    public void lataaRahaa(double amount) {
-        this.balance += amount;
-    }
-
-    public String toString() {
-        return "The card has a balance of " + balance + " euros";
+    public boolean takeMoney(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount; 
+            return true; 
+        } else {
+            return false;
+        }
     }
 }
