068d9f48c554e3e1d59765c7217930c6a7455714
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2024-12-05 13:06:08 +0000

    person manager part 2 (ne pabeigt)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,24 +4,19 @@ import java.io.*;
 import java.nio.file.StandardOpenOption;
 
 public class App {
-    public static void main(String[] args) {
-        PaymentTerminal unicafeExactum = new PaymentTerminal();
-        System.out.println(unicafeExactum);
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
 
-        PaymentCard annesCard = new PaymentCard(2);
-
-        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
-
-        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-
-        unicafeExactum.addMoneyToCard(annesCard, 100);
-
-        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-
-        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
-
-        System.out.println(unicafeExactum);
+        boolean isProgramRunning = true;
+        while (isProgramRunning) {
+            String command = scanner.nextLine();
+            
+            if (command.equals("exit")) {
+                System.out.println("Thanks, bye bye!");
+                isProgramRunning = false;
+            } else if (command.equals("show")){
+                System.out.println(PersonManager.getPersonList());
+            }
+        }
     }
 }
