dd1713158e007ec5f6e3b2cfe1647f08443f6678
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2025-01-16 09:17:55 +0000

    person and subclasses part 4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
        
-        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-        System.out.println(ollie);
-        ollie.study();
-        System.out.println(ollie);
+        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+System.out.println(ada);
+System.out.println(esko);
+
+Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+
+int i = 0;
+while (i < 25) {
+  ollie.study();
+  i = i + 1;
+}
+System.out.println(ollie);
     }
     }
     
---
+++ src/main/java/lv/rvt/Teacher.java
@@ -0,0 +1,17 @@
+package lv.rvt;
+
+public class Teacher extends Person {
+    private int salary;
+
+    // Constructor
+    public Teacher(String name, String address, int salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+    // Overriding the toString method for Teacher
+    @Override
+    public String toString() {
+        return super.toString() + "\n  salary " + this.salary + " euro/month";
+    }
+}
