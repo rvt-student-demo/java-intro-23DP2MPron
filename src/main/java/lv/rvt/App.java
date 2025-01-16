edbcbcd5be0539fd752b303e87c7fa12d37bacce
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2025-01-16 09:10:50 +0000

    person and subclasses part 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,26 +3,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-        public static void main(String[] args) {
-            SimpleDate date = new SimpleDate(24, 3, 2017);
-            SimpleDate date2 = new SimpleDate(23, 7, 2017);
-    
-            Person leo = new Person("Leo", date, 62, 9);
-            Person lily = new Person("Lily", date2, 65, 8);
-    
-            if (leo.equals(lily)) {
-                System.out.println("Is this quite correct?");
-            } else {
-                System.out.println("Leo and Lily are not the same.");
-            }
-    
-            Person leoWithDifferentWeight = new Person("Leo", date, 62, 10);
-    
-            if (leo.equals(leoWithDifferentWeight)) {
-                System.out.println("Is this quite correct?");
-            } else {
-                System.out.println("Leo with different weight is not the same as the original Leo.");
-            }
-        }
+    public static void main(String[] args) {
+       
+        Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
+        Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+
+       
+        System.out.println(ada);
+        System.out.println(esko);
+    }
     }
     
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -3,53 +3,28 @@ package lv.rvt;
 
 public class Person {
     private String name;
-    private SimpleDate birthday;
-    private int height;
-    private int weight;
+    private String address;
 
-    public Person(String name, SimpleDate birthday, int height, int weight) {
+   
+    public Person(String name, String address) {
         this.name = name;
-        this.birthday = birthday;
-        this.height = height;
-        this.weight = weight;
+        this.address = address;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public SimpleDate getBirthday() {
-        return birthday;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
+   
     @Override
-    public boolean equals(Object compared) {
-        
-        if (this == compared) {
-            return true;
-        }
+    public String toString() {
+        return name + "\n  " + address;
+    }
 
-        
-        if (!(compared instanceof Person)) {
-            return false;
-        }
+    
+    public static void main(String[] args) {
+        Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
+        Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
 
-        
-        Person comparedPerson = (Person) compared;
-
-       
-        return this.name.equals(comparedPerson.name)
-                && this.birthday.equals(comparedPerson.birthday)
-                && this.height == comparedPerson.height
-                && this.weight == comparedPerson.weight;
+        System.out.println(ada);
+        System.out.println(esko);
     }
 }
+
 
