94a3603cbd0cbd36e79f4865e64a611007fe9f73
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2025-01-16 09:12:59 +0000

    person and subclasses part 2

--- src/main/java/lv/rvt/Student.java
+++ src/main/java/lv/rvt/Student.java
@@ -1,6 +1,32 @@
 package lv.rvt;
 import java.util.*;
 
-public class Student {
+public class Student extends Person {
+    private int credits;
 
+    
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+
+    
+    public void study() {
+        this.credits++;
+    }
+
+    
+    public int credits() {
+        return this.credits;
+    }
+
+    
+    public static void main(String[] args) {
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+        System.out.println(ollie);
+        System.out.println("Study credits " + ollie.credits());
+        ollie.study();
+        System.out.println("Study credits " + ollie.credits());
+    }
 }
+
