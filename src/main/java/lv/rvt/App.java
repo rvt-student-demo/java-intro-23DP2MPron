63c56b08446d4d98647bd30c49703dc22b971d4e
23DP2MPron <166109717+23DP2MPron@users.noreply.github.com>
2025-01-29 11:19:54 +0000

    Warehouse 7 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,19 +4,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-     public static void main(String[] args) {
-        ArrayList<Person> persons = new ArrayList<>();
-        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
-
-        printPersons(persons);
-    }
-
-    // Method to print all persons in the list
-    public static void printPersons(ArrayList<Person> persons) {
-        for (Person person : persons) {
-            System.out.println(person);
-        }
+    public static void main(String[] args) {
+		ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("juice", 1000.0, 1000.0);
+		juice.takeFromWarehouse(11.3); 
+        juice.addToWarehouse(1.0);
+		juice.printAnalysis();
     }
     }
     
